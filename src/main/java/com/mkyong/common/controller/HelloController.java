29e9a24485b3b0ba4d2d1c737ffa574e916742c0
César Fernández <cesar.fg@gmail.com>
2019-11-18 13:40:32 +0100

    Update HelloController.java

--- src/main/java/com/mkyong/common/controller/HelloController.java
+++ src/main/java/com/mkyong/common/controller/HelloController.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HelloController {
  
 	@RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model2) {
+	public String printWelcome(final ModelMap model) {
  
-		model2.addAttribute("message", "Spring Security Hello World");
+		model.addAttribute("message", "Spring Security Hello World");
 		return "hello";
  
 	}
