10451db36a2d2607e7720cc7edd0d94f5c03a5d7
César Fernández <cesar.fg@gmail.com>
2019-11-19 13:15:02 +0100

    Update HelloController.java

--- src/main/java/com/mkyong/common/controller/HelloController.java
+++ src/main/java/com/mkyong/common/controller/HelloController.java
@@ -1,18 +1,18 @@
 package com.mkyong.common.controller;
-
+  
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
+  
 /**
  * Represents HelloController.
  */
 @Controller
 @RequestMapping("/welcome")
 public final class HelloController {
-
+  
  /**
  * printWelcome.
  * @param  model
